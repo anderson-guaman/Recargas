dc993a58d52afdc8aaeec98685c949f87d8ea1f7
Anderson Guaman <anderson.guaman@udla.edu.ec>
2024-06-06 08:25:29 -0500

    proyecto ok

--- Recargas/MainPage.xaml.cs
+++ Recargas/MainPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace Recargas
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Recargas
 {
     public partial class MainPage : ContentPage
     {
@@ -21,11 +26,27 @@
 
             if (confirmacion)
             {
-                
+
+                string numeroCelular = AGnumeroCelular.Text;
+                string valorSeleccionado = AGlabelSeleccion.Text.Replace("Ha seleccionado una recarga de: ", "").Replace(" dólares", "");
+                string fecha = DateTime.Now.ToString("dd/MM/yyyy");
+                string contenido = $"Se hizo una recarga al numero {numeroCelular} de {valorSeleccionado} en la siguiente fecha: {fecha}";
+
+                await Guardar(numeroCelular, contenido);
+
                 await DisplayAlert("Recarga", "Recarga realizada exitosamente", "OK");
             }
 
         }
+        private async Task Guardar(string nombreArchivo, string contenido)
+        {
+            string filePath = Path.Combine(FileSystem.AppDataDirectory, $"{nombreArchivo}.txt");
+            using (StreamWriter writer = new StreamWriter(filePath, false))
+            {
+                await writer.WriteLineAsync(contenido);
+            }
+        }
+
     }
 
 }
